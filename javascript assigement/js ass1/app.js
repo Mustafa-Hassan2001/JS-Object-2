34a8f4763b3ff6054d6f51f4deb8991caf1df6ae
Mustafa-Hassan2001 <98483655+Mustafa-Hassan2001@users.noreply.github.com>
2022-01-27 21:44:39 +0500

    Add files via upload

---
+++ javascript assigement/js ass1/app.js
@@ -0,0 +1,134 @@
+// ch#1----------"ALERTS"-------------
+
+//q.1:-
+//prompt("Hello!\n How was our website greet it 1-10");
+
+//q2:-
+//alert("Error! Please enter valid password");
+
+//q.3:-
+//alert("Welcome to JS Land... \n Happy Coading!  ");
+
+//q.4:-
+//alert("Welcome to JS Land...");
+
+//q.5:-
+//alert("Happy Coading!");
+
+//==========================================================
+
+// ch#2 ------------VARIABLES FOR STRINGS--------
+
+//1.
+// var  username;
+
+//2.
+//var myname = "Syed Mustafa Hassan";
+ 
+//1.
+//var message = "Hello World";
+//alert(message);
+
+//2.
+//var name = prompt("Enter name");
+//alert(name);
+
+//3.
+//var age = prompt("Enter Age");
+//alert(age,"years old");
+
+//4.
+//alert("Certified Moblie Appilication Development");
+
+//5.
+//alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+//6.
+//var email = prompt("Enter email");
+//alert("My email is", email);
+
+//7.
+//var book = "A smarter way to learn JavaScript";
+//alert("I try to learn javascript from the Book",book);
+
+//8.
+//document.write("Yah! I can write HTML contact by JavaScript");
+
+
+//9.
+//var show = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”" ;
+//alert(show);
+
+// ch#3 ====================  VARIABLES FOR NUMBERS=========================
+
+
+//1.
+// var age = ("I am 15 years old");
+// alert(age);
+
+//2.
+// var visits = 13 ;
+// alert("you have visited this site 13 times" );
+
+//3.
+// var birthYear = 2001;
+// document.write("My birth year is", birthYear);
+
+//4.
+// var user_name = prompt("Enter User Name");
+// var product = prompt("Enter product title = T-shirt(s), T-shirts(l) ,etc");
+// var Quantity = prompt("Enter Quantity");
+// var ordered = "ordered";
+// var onXYZClothingstore = "on XYZ Clothing store";
+
+// document.write(user_name,"ordered" , Quantity, product, "on XYZ Clothing store" );
+
+
+//=---------------------------------------------
+
+// ch#4 VARIABLE NAMES: LEGAL & ILLEGAL==================================
+
+//1.
+//var name , place , animal
+
+
+
+//2.
+//legal :-
+// var name 
+// var place
+// var animal 
+// var thing
+// var noun
+
+//lilegal :-
+// var 12mam;
+// var #nsp;
+// var (2mc90
+// var @1!;
+// var {]};
+
+
+//3.
+// document.write("Variable names can only contain, numbers, $ and _. For example $my_1stVariable");
+// document.write("Variables must begin with a letter, $ or _ . For example $name, _name or name");
+// document.write("Variable names are case sensitive");
+// document.write("Variable names should not be JS Keywords");
+
+
+
+//===============================
+
+//    chap#5:- 
+//1.
+// document.write("Sum of 3 and 5 is ", 3+5);
+// document.write("Subraction of 3 and 5 is ", 3-5);
+// document.write("Division of 3 and 5 is ", 3/5);
+// document.write("Modulator of 3 and 5 is ", 3%5);
+
+//2.
+
+
+
+
+
